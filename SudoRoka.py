f8c4e0fd3a33cae63960ad143810f3003bcf8bb2
Oliver Juhasz <oliver.juhasz@outlook.com>
2018-10-03 23:44:16 +0200

    Added color-matrix generator
    Color-matrix generator added as a possible solution to the color-problem, needs testing. Master file not changed.

--- SudoRoka.py
+++ SudoRoka.py
@@ -2,59 +2,46 @@ import os
 import platform
 import random
 import time
+from colorama import init
+from colorama import Fore, Back, Style
+   
 
-def check_validation():
-    ValidationCheck = 0  
-    for i in range(0,9):
-        if len(list(filter(lambda x: 10 > x > 0,MatrixList2[i]))) == 9:
-            ValidationCheck += 1
-        templist = [[i2][0] for i2 in range(0,9)]
-        if len(list(filter(lambda x: 10 > x > 0,templist))):
-            ValidationCheck += 1
-    return True if ValidationCheck == 18 else False
+def color_generator(board):
+    colortrix = [[1,8,4,9,6,3,7,2,5],[5,6,2,7,4,8,3,1,9],[3,9,7,5,1,2,8,6,4],[2,3,9,6,5,7,1,4,8],[7,5,6,1,8,4,2,9,3],[4,1,8,2,3,9,6,5,7],[9,4,1,3,7,6,5,8,2],[6,2,3,8,9,5,4,7,1],[8,7,5,4,2,1,9,3,6]]
+    for i, elem in enumerate(board):
+        for i2, subelem in enumerate(elem):
+            if subelem == 1:
+                colortrix[i][i2] = f"{Fore.CYAN}1{Style.RESET_ALL}"
+            elif subelem == 2:
+                colortrix[i][i2] = f"{Fore.RED}2{Style.RESET_ALL}"                
+            elif subelem == 3:
+                colortrix[i][i2] = f"{Fore.GREEN}3{Style.RESET_ALL}"     
+            elif subelem == 4:
+                colortrix[i][i2] = f"{Fore.YELLOW}4{Style.RESET_ALL}"    
+            elif subelem == 5:
+                colortrix[i][i2] = f"{Fore.BLUE}5{Style.RESET_ALL}"    
+            elif subelem == 6:
+                colortrix[i][i2] = f"{Fore.MAGENTA}6{Style.RESET_ALL}"    
+            elif subelem == 7:
+                colortrix[i][i2] = f"{Fore.WHITE}7{Style.RESET_ALL}"    
+            elif subelem == 8:
+                colortrix[i][i2] = f"{Fore.BLUE}8{Style.RESET_ALL}"                                                                            
+            elif subelem == 9:
+                colortrix[i][i2] = f"{Fore.GREEN}9{Style.RESET_ALL}"                    
+            else:
+                colortrix[i][i2] = " "                  
+    return colortrix
+    
+MatrixList = [[1,8,4,9,6,3,7,2,5],[5,6,2,7,4,8,3,1,9],[3,9,7,5,1,2,8,6,4],[2,3,9,6,5,7,1,4,8],[7,5,6,1,8,4,2,9,3],[4,1,8,2,3,9,6,5,7],[9,4,1,3,7,6,5,8,2],[6,2,3,8,9,5,4,7,1],[8,7,5,4,2,1,9,3,6]]
+colorstring = ""
+color = color_generator(MatrixList)
 
-def check_verification():
-    check_verification = 0
-    for i in range(0,9):
-        if len(set(MatrixList2[i])) == len(MatrixList2[i]):
-            check_verification += 1
-
-    templist = []
-    for i in range(0,9):
-        templist.clear()
-        for i2 in range(0,9):
-            templist.append(MatrixList2[i2][i])
-        if len(set(templist)) == len(templist):
-            check_verification += 1
-
-    for block in range(0,7,3):
-        for block2 in range(0,7,3):
-            templist.clear()
-            for i in range(0,3):
-                for i2 in range(0,3):
-                    templist.append(MatrixList2[i2 + block][i + block2])
-            if len(set(templist)) == len(templist):
-                check_verification += 1
-    if check_verification == 27:
-        return True
-    else:
-        return False
+for elem in color:
+    colorstring = ""
+    for subelem in elem:
+        colorstring += subelem
+    print(colorstring)
 
 
-MatrixList = [\
-[1,8,4,9,6,3,7,2,5],
-[5,6,2,7,4,8,3,1,9],
-[3,9,7,5,1,2,8,6,4],
-[2,3,9,6,5,7,1,4,8],
-[7,5,6,1,8,4,2,9,3],
-[4,1,8,2,3,9,6,5,7],
-[9,4,1,3,7,6,5,8,2],
-[6,2,3,8,9,5,4,7,1],
-[8,7,5,4,2,1,9,3,6]]
-
-MatrixList2 = MatrixList
-
-print(check_validation())
-print(check_verification())
 
 #list(sorted(inventory.items(), key=lambda x: x[1],reverse=False))
