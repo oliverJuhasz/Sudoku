c20d2f8345009316c3e3d8f54f4bf3b6bbfa1d56
Oliver Juhasz <oliver.juhasz@outlook.com>
2018-10-04 14:55:25 +0200

    Duplicate colorgenerator prototype added to SUdoRoka

--- SudoRoka.py
+++ SudoRoka.py
@@ -1,47 +1,49 @@
-import os
-import platform
-import random
-import time
-from colorama import init
-from colorama import Fore, Back, Style
-   
 
-def color_generator(board):
-    colortrix = [[1,8,4,9,6,3,7,2,5],[5,6,2,7,4,8,3,1,9],[3,9,7,5,1,2,8,6,4],[2,3,9,6,5,7,1,4,8],[7,5,6,1,8,4,2,9,3],[4,1,8,2,3,9,6,5,7],[9,4,1,3,7,6,5,8,2],[6,2,3,8,9,5,4,7,1],[8,7,5,4,2,1,9,3,6]]
-    for i, elem in enumerate(board):
-        for i2, subelem in enumerate(elem):
-            if subelem == 1:
-                colortrix[i][i2] = f"{Fore.CYAN}1{Style.RESET_ALL}"
-            elif subelem == 2:
-                colortrix[i][i2] = f"{Fore.RED}2{Style.RESET_ALL}"                
-            elif subelem == 3:
-                colortrix[i][i2] = f"{Fore.GREEN}3{Style.RESET_ALL}"     
-            elif subelem == 4:
-                colortrix[i][i2] = f"{Fore.YELLOW}4{Style.RESET_ALL}"    
-            elif subelem == 5:
-                colortrix[i][i2] = f"{Fore.BLUE}5{Style.RESET_ALL}"    
-            elif subelem == 6:
-                colortrix[i][i2] = f"{Fore.MAGENTA}6{Style.RESET_ALL}"    
-            elif subelem == 7:
-                colortrix[i][i2] = f"{Fore.WHITE}7{Style.RESET_ALL}"    
-            elif subelem == 8:
-                colortrix[i][i2] = f"{Fore.BLUE}8{Style.RESET_ALL}"                                                                            
-            elif subelem == 9:
-                colortrix[i][i2] = f"{Fore.GREEN}9{Style.RESET_ALL}"                    
-            else:
-                colortrix[i][i2] = " "                  
-    return colortrix
+MatrixList = [
+[1,8,4,9,6,3,7,2,5],
+[5,6,2,7,4,8,3,1,9],
+[3,9,7,5,1,2,8,6,4],
+[2,3,9,6,5,7,1,4,8],
+[7,5,6,1,8,4,2,9,3],
+[4,1,8,2,3,9,6,5,7],
+[9,4,1,3,7,6,5,8,2],
+[6,2,3,8,9,5,4,7,1],
+[8,7,5,4,2,1,9,3,6]]
+
+print(*MatrixList,sep="\n")
+
+def color_checker(board,x,y):
+    checked_number = board[x][y]
+    templist = []
     
-MatrixList = [[1,8,4,9,6,3,7,2,5],[5,6,2,7,4,8,3,1,9],[3,9,7,5,1,2,8,6,4],[2,3,9,6,5,7,1,4,8],[7,5,6,1,8,4,2,9,3],[4,1,8,2,3,9,6,5,7],[9,4,1,3,7,6,5,8,2],[6,2,3,8,9,5,4,7,1],[8,7,5,4,2,1,9,3,6]]
-colorstring = ""
-color = color_generator(MatrixList)
+    #horizontal check
+    if board[x].count(checked_number) > 1:
+        print("Horizontal duplication")
+    #vertical check
+    templist = list(board[i][0] for i in range(0,9))
+    if templist.count(checked_number) > 1:
+        print("Vertical duplication")
+    #block check
+    templist = []
+    blockIDx = 0
+    blockIDy = 0
+    if x < 4:
+        blockIDx = 0
+    elif x < 7:
+        blockIDx = 3
+    elif x < 10:
+        blockIDx = 6
+    if y < 4:
+        blockIDy = 0
+    elif y < 7:
+        blockIDy = 3
+    elif y < 10:
+        blockIDy = 6    
+    for i in range(0 + blockIDy, 3 + blockIDy):
+        for i2 in range(0 + blockIDx, 3 + blockIDx):
+            templist.append(board[i][i2])
+    if templist.count(checked_number) > 1:
+        print("Block duplication")
 
-for elem in color:
-    colorstring = ""
-    for subelem in elem:
-        colorstring += subelem
-    print(colorstring)
 
-
-
-#list(sorted(inventory.items(), key=lambda x: x[1],reverse=False))
+color_checker(MatrixList,1,8)
